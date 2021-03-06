5e003b2b0a421f4423435c1600d484aaeb57a71d
Thai-fi <d.egorov66@gmail.com>
2021-03-06 21:53:19 +0500

    add Navbar

--- lr12-react/src/App.js
+++ lr12-react/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
-    <div className="container">
-			<h1>test</h1>
-    </div>
+    <BrowserRouter>
+			<Navbar />
+			<div className="container pt-4">
+				<Switch>
+					<Route path={'/'} exact component={Home}/>
+					<Route path={'/about'} exact component={About}/>
+				</Switch>
+			</div>
+		</BrowserRouter>
   );
 }
 
