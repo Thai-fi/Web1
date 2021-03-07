08e7ce4a4d1b01b417894e99b9cf3e78b432f9ce
Thai-fi <d.egorov66@gmail.com>
2021-03-07 11:53:58 +0500

    add animation and fix bugs

--- lr12-react/src/components/Notes.js
+++ lr12-react/src/components/Notes.js
@@ -1,26 +1,31 @@
 import React from 'react';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
-export const Notes = ({notes}) => {
-	return (
-		<ul className="list-group">
+export const Notes = ({notes, onRemove}) => (
+	<TransitionGroup
+		component="ul"
+		className="list-group">
 			{notes.map(note => (
-				<li
-					className="list-group-item note "
+				<CSSTransition
 					key={note.id}
+					classNames={'note'}
+					timeout={800}
 				>
+					<li className="list-group-item note ">
 					<div>
 						<strong>{note.title}</strong>
-						<small>{new Date().toLocaleDateString()}</small>
+						<small>{note.date}</small>
 					</div>
 
 					<button
 						type="button"
-						class="btn btn-outline-danger btn-sm"
+						className="btn btn-outline-danger btn-sm"
+						onClick={() => onRemove(note.id)}
 					>
 						&times;
 					</button>
 				</li>
+				</CSSTransition>
 			))}
-		</ul>
-	)
-}
+		</TransitionGroup>
+)
--- lr12-react/src/context/firebase/FirebaseState.js
+++ lr12-react/src/context/firebase/FirebaseState.js
@@ -2,7 +2,7 @@ import React, {useReducer} from 'react';
 import axios from 'axios';
 import { FirebaseContext } from './firebaseContext';
 import { firebaseReducer } from './firebaseReducer';
-import { REMOVE_NOTE, SHOW_LOADER } from '../types';
+import { ADD_NOTE, FETCH_NOTES, REMOVE_NOTE, SHOW_LOADER } from '../types';
 
 //не работает env.
 const url = 'https://lr12-reactjs-default-rtdb.firebaseio.com'
@@ -21,7 +21,14 @@ export const FirebaseState = ({children}) => {
 		showLoader()
 		const res = await axios.get(`${url}/notes.json`)
 
-		console.log('fetchNotes', res.data)
+		const payload = Object.keys(res.data).map( key => {
+			return{
+				...res.data[key],
+				id: key
+			}
+		})
+
+		dispatch({type: FETCH_NOTES, payload})
 	}
 
 	const addNote = async title => {
@@ -30,7 +37,11 @@ export const FirebaseState = ({children}) => {
 		}
 		try{
 			const res = await axios.post(`${url}/notes.json`, note)
-			console.log('addNote', res.data)
+			const payload = {
+				...note,
+				id: res.data.name
+			}
+			dispatch({type: ADD_NOTE, payload})
 		} catch (e){
 			throw new Error(e.message)
 		}
